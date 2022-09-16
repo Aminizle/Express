2f0e182e36d85d79266b053f039f73499e513b5b
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2022-09-16 00:33:45 -0400

    added params

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,35 @@ router.get('/new', (req, res) => {
     res.send('New users form')
 })
 
+router
+.route('/:id')
+.get((req, res) => {
+    console.log(req.user)
+    res.send(`Get users with the id: ${req.params.id}`)
+})
+.put((req, res) => {
+    res.send(`Update users with the id: ${req.params.id}`)
+})
+.delete((req, res) => {
+    res.send(`Delete users with the id: ${req.params.id}`)
+})
+
+const users = [{ name: "Kyle" },  { name: "Sally" }]
+
+router.param('id', (req, res, next, id) => {
+    req.user = users[id]
+    next()
+})
+
+
+// router.get('/:id', (req, res) => {
+//     res.send(`Get users with the id: ${req.params.id}`)
+// })
+// router.put('/:id', (req, res) => {
+//     res.send(`Update users with the id: ${req.params.id}`)
+// })
+// router.delete('/:id', (req, res) => {
+//     res.send(`Delete users with the id: ${req.params.id}`)
+// })
+
 module.exports = router
